4f05cc8e004fd3762ebe219d1bfa403d33720f51
Erik Dackebro <erik.dackebro@gmail.com>
2018-02-26 15:11:15 +0100

    test: Add DIY branch coverage (resolves #4)
    DIY branch coverage for PokerSinglePlayer::showWinnerAlert().
    With the given tests we get 28% branch coverage.

--- src/edu/ucsb/cs56/projects/games/poker/PokerSinglePlayer.java
+++ src/edu/ucsb/cs56/projects/games/poker/PokerSinglePlayer.java
@@ -159,12 +159,12 @@ class PokerSinglePlayer extends PokerGameGui {
 	*/
 	public void showWinnerAlert() {
 		if(!gameOver){
-
+TestBench.BranchReached("showWinnerAlert0");
 			String message = "";
 			oSubPane2.remove(backCardLabel1);
 			oSubPane2.remove(backCardLabel2);
 			for(int i=0;i<2;i++){
-
+TestBench.BranchReached("showWinnerAlert1");
 				oSubPane2.add(new JLabel(getCardImage((opponent.getHand()).get(i))));
 			}
 			updateFrame();
@@ -172,25 +172,25 @@ class PokerSinglePlayer extends PokerGameGui {
 			message = winningHandMessage();
 
 			if (winnerType == Winner.PLAYER) {
-
+TestBench.BranchReached("showWinnerAlert2");
 				System.out.println("player");
 				message = message + ("\n\nYou win!\n\nNext round?");
 			} else if (winnerType == Winner.OPPONENT) {
-
+TestBench.BranchReached("showWinnerAlert3");
 				System.out.println("opponent");
 				message = message + ("\n\nOpponent wins.\n\nNext round?");
 			} else if (winnerType == Winner.TIE){
-
+TestBench.BranchReached("showWinnerAlert4");
 				System.out.println("tie");
 				message = message + ("\n\nTie \n\nNext round?");
 			} else {
-
+TestBench.BranchReached("showWinnerAlert5");
 			}
 
 			int option = showWinnerConfirmDialog(message);
 
 			if (option == JOptionPane.YES_OPTION) {
-
+TestBench.BranchReached("showWinnerAlert6");
 				// Restart
 				mainFrame.dispose();
 				PokerSinglePlayer singlePlayerReplay;
@@ -198,31 +198,31 @@ class PokerSinglePlayer extends PokerGameGui {
 				// Check if players have enough chips.
 				// Create new game.
 				if(player.getChips() < 5 || opponent.getChips() < 5){
-
+TestBench.BranchReached("showWinnerAlert7");
 					JOptionPane.showMessageDialog(null, "Resetting chips...");
 					singlePlayerReplay = new PokerSinglePlayer();
 					singlePlayerReplay.go();
 				} else {
-
+TestBench.BranchReached("showWinnerAlert8");
 					singlePlayerReplay = new PokerSinglePlayer(player.getChips(),opponent.getChips());
 					singlePlayerReplay.go();
 				}
 			} else if (option == JOptionPane.NO_OPTION) {
-
+TestBench.BranchReached("showWinnerAlert9");
 				if(player.getChips() < 5 || opponent.getChips() < 5) {
-
+TestBench.BranchReached("showWinnerAlert10");
 					gameOver("GAME OVER! No chips left!");
 				} else {
-
+TestBench.BranchReached("showWinnerAlert11");
 				}
 				gameOver("GAME OVER! Thanks for playing.\n\n");
 			} else {
-
+TestBench.BranchReached("showWinnerAlert12");
 				// Quit
 				System.exit(1);
 			}
 		} else {
-
+TestBench.BranchReached("showWinnerAlert13");
 		}
 	}
 
